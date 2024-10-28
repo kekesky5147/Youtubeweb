c8f24288fd814e9d55116831ac6502ee1c015a39
Minseok Kim^C <ksj5147@gmail.com>
2024-10-28 12:33:45 -0700

    setting

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
+    {/* from App.js */}
     <App />
   </React.StrictMode>
 )
